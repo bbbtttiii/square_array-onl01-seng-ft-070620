1ae7943c13be8c29d4df3ff191f7c8d5516636fe
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 17:40:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,4 @@
-# def square_array(array)
-#   numbers = [1,2,3]
-#   numbers.each do |square|
-
-#   end
-# end
 
 def square_array(array)
-  new_array = []
-  array.each do |x|
-    array << x ** 2
-  end
-  new_array
+  
 end
