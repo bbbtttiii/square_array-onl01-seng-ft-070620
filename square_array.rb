cd8f4e88b68991f9843ad040e77c7fbccc93fd2c
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 17:48:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  squared = []
-  array.each do |x|
-    squared << x ** 2
+  squared = []           #create empty array for squared numbers
+  array.each do |x|      #iterate on each number in original array using x as the placeholder
+    squared << x ** 2    #add each squared iteration into the empty squared array
   end
-  squared
+  squared                #return the squared array
 end
