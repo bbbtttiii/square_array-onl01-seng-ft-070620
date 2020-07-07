7ceeebe6b56cf377f7257a03f3ed771633d381f5
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 17:50:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def square_array(array)  #method which takes an array of numbers as an argument
+  squared = []           #create empty array for squared numbers
+  array.each do |x|      #iterate on each number in original array using x as the placeholder
+    squared << x ** 2    #add each squared iteration into the empty squared array
+  end
+  squared                #return the squared array
 end
