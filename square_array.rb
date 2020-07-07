ab22e42e3022f81942c5c0111f3f16446fb9e7be
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 17:35:48 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+# def square_array(array)
+#   numbers = [1,2,3]
+#   numbers.each do |square|
+
+#   end
+# end
+
 def square_array(array)
-  # your code here
+  array = []
+  array.each{ |x| array << i ** 2}
 end
